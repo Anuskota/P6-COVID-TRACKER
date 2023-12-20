b167b44080c474128f933e3df6de64c06092255d
Gabriela Farinha <vallefarinha@gmail.com>
2023-12-20 16:55:38 -0300

    fixing navbar and adding animation

--- src/components/molecules/Navbar.jsx
+++ src/components/molecules/Navbar.jsx
@@ -9,8 +9,9 @@ const Navbar = () => {
         <nav>
             <div>
                 <div className="nav-container">
+                    <div className="logo"><a href="#"><img className="animate-spin" src={coronavirus} alt="coronavirus"/></a></div>
+               
                     <ul className="nav-items-container">
-                        <li className="logo"><a href="#"><img src={coronavirus} alt="coronavirus"/></a></li>
                         <li><a href="#" className="active nav-content-bttn" data-tab="chats"><i className="bi bi-pie-chart"></i></a></li>
                         <li><a href="#" className="nav-content-bttn" data-tab="table"><i className="bi bi-list"></i></a></li>
                         <li><a href="#" className="nav-content-bttn" data-tab="tiles"><i className="bi bi-grid"></i></a></li>
@@ -18,9 +19,11 @@ const Navbar = () => {
                         <li><a href="#" className="nav-content-bttn" data-tab="widgets"><i className="bi bi-layers"></i></a></li>
                         <li><a href="#" className="nav-content-bttn" data-tab="charts"><i className="bi bi-graph-up-arrow"></i></a></li>
                         <li><a href="#" className="nav-content-bttn" data-tab="continent" id="continentTab"><i className="bi bi-globe"></i></a></li>
-                        <li className="flex-lg-brackets"><a href="#" data-tab="blog" className="nav-content-bttn"><i className="bi bi-chat-left"></i></a></li>
-                        <li className="nav-content-bttn"><a href="#"><i className="bi bi-pause-circle"></i></a></li>
+                        <li><a href="#" data-tab="blog" className="nav-content-bttn"><i className="bi bi-chat-left"></i></a></li>
                     </ul>
+                    <div className="last-icon-nav"><li><a href="#" data-tab="blog" className="nav-content-bttn"><i className="bi bi-pause-circle"></i></a></li>
+                    </div>
+
                 </div>
             </div>
         </nav>
