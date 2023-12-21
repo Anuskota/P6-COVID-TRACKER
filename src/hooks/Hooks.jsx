a09dfaa70c9350d64eea824d6a5203d8d1fe6125
Gabriela Farinha <vallefarinha@gmail.com>
2023-12-21 11:06:29 -0300

    add variable endpoint

--- src/hooks/Hooks.jsx
+++ src/hooks/Hooks.jsx
@@ -1,26 +1,28 @@
 import { useState, useEffect } from 'react';
-import roots from './services/roots';
+import fetchData from '../services/fetchData';
 
 const Hooks = () => {
   const [apiData, setApiData] = useState(null);
   const [error, setError] = useState(null);
 
+  const apiEndpoint = 'countries'; 
+
   useEffect(() => {
-    roots()
+    fetchData(apiEndpoint) 
       .then((api) => {
         setApiData(api);
       })
       .catch((error) => {
         setError(error);
       });
-  }, []); 
+  }, [apiEndpoint]); 
 
   return (
     <>
       {error ? (
         <h1>Error: {error.message}</h1>
       ) : (
-        apiData && <img src={apiData} alt="API Image" />
+        apiData && <h1 src={apiData} alt="API Image" />
       )}
     </>
   );
