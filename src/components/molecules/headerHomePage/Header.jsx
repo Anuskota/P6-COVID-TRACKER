50a97f16a1723999f77ba28483d06e04e6aba2c2
AnaCe-7 <ana.cecilia.reques@gmail.com>
2023-12-30 22:31:11 +0100

    Update files in branch feature/headerHomePage

---
+++ src/components/molecules/headerHomePage/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./header.css"
+
+
+const Header = () => {
+  return (
+    <header>
+    <div className="info-header-container">
+        <h2>Download the covid app for get latest update</h2>
+        <p>Human coronaviruses are common and are typically associated with mild illnesses, similar to the common cold. The corona virus COVID-19 is affecting 210 countries & territories arount the world and 2 international conveyances.</p>
+        <button>HOW TO PROTECT</button>
+    </div>
+    <div className="image-header-container">
+        <img src="" alt="Image of the application displayed on a mobile phone" />
+
+    </div>
+</header>
+  )
+}
+
+export default Header
