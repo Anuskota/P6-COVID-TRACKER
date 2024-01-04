a4787cfa0c88daa6a9eaa916ab35de886e240ba0
Anuskota <anamangada.dev@gmail.com>
2024-01-04 16:25:48 +0100

    new branch feature/navbarHomePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Footer from "../components/molecules/footer/Footer"
 import Header from "../components/molecules/headerHomePage/Header";
+import NavbarHome from "../components/molecules/navbarHomePage/NavbarHome";
 
 const Home = () => {
   return (
     <>
-    <div>Home</div>
+      <div>Home</div>
+    <NavbarHome/>  
     <Header/>
     <Footer/>
     </>
