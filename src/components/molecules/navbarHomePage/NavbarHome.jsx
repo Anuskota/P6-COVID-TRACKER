09d41df8446211684cf9c21d2840e10c6f00d2e0
Anuskota <anamangada.dev@gmail.com>
2024-01-04 20:07:49 +0100

    remove and star one more time de navbar

--- src/components/molecules/navbarHomePage/NavbarHome.jsx
+++ src/components/molecules/navbarHomePage/NavbarHome.jsx
@@ -23,35 +23,35 @@ const NavbarHome = () => {
                     <span className="navbar-toggler-icon"/>
                   </button>
                   <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul className="navbar-nav">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                       <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Home</a>
-                        <div className="dropdown-menu">
-                                                <a className="dropdown-item" href="">Home One</a>
-                                                <a className="dropdown-item" href="">Home Two</a>
-                                                <a className="dropdown-item" href="">Home Three</a>
-                                                <a className="dropdown-item" href="">Home Four</a>
-                                                <a className="dropdown-item" href="">Home Five</a>
-                                                <a className="dropdown-item" href="">Home Dark One</a>
-                                                <a className="dropdown-item" href="">Home Dark Two</a>
-                                                <a className="dropdown-item" href="">Home Dark Three</a>
-                                                <a className="dropdown-item" href="">Home Dark Four</a>
-                                                <a className="dropdown-item" href="">Home dark Five</a>
-                                                <a className="dropdown-item" href="">Onepage</a>
-                                                <a className="dropdown-item" href="">Onepage Dark</a>
-                                                <a className="dropdown-item" href="">Tracker</a>
-                                            </div>
+                        <a className="nav-link dropdown-toggle" href="#"id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Home</a>
+                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                                <li><a className="dropdown-item" href="">Home One</a></li>
+                                                <li><a className="dropdown-item" href="">Home Two</a></li>
+                                                <li><a className="dropdown-item" href="">Home Three</a></li>
+                                                <li><a className="dropdown-item" href="">Home Four</a></li>
+                                                <li><a className="dropdown-item" href="">Home Five</a></li>
+                                                <li><a className="dropdown-item" href="">Home Dark One</a></li>
+                                                <li><a className="dropdown-item" href="">Home Dark Two</a></li>
+                                                <li><a className="dropdown-item" href="">Home Dark Three</a></li>
+                                                <li><a className="dropdown-item" href="">Home Dark Four</a></li>
+                                                <li><a className="dropdown-item" href="">Home dark Five</a></li>
+                                                <li><a className="dropdown-item" href="">Onepage</a></li>
+                                                <li><a className="dropdown-item" href="">Onepage Dark</a></li>
+                                                <li><a className="dropdown-item" href="">Tracker</a></li>
+                          </ul>
                       </li>
                       <li className="nav-item"><a className="nav-link" href="">Prevention</a></li>
                       <li className="nav-item"><a className="nav-link" href="">Qurantine</a></li>
                       <li className="nav-item dropdown">
                                             <a className="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Pages</a>
-                                            <div className="dropdown-menu">
-                                                <a className="dropdown-item" href="">Hand Wash</a>
-                                                <a className="dropdown-item" href="">SocialDistance</a>
-                                                <a className="dropdown-item" href="">Blog</a>
-                                                <a className="dropdown-item" href="">Blog Details</a>
-                                            </div>
+                                            <ul className="dropdown-menu">
+                                                <li><a className="dropdown-item" href="">Hand Wash</a></li>
+                                                <li><a className="dropdown-item" href="">SocialDistance</a></li>
+                                                <li> <a className="dropdown-item" href="">Blog</a></li>
+                                                <li><a className="dropdown-item" href="">Blog Details</a></li>
+                                            </ul>
                       </li>
                       <li className="nav-item"><a className="nav-link" href="">About</a></li>
                       <li className="nav-item"><a className="nav-link" href="">Help</a></li>
