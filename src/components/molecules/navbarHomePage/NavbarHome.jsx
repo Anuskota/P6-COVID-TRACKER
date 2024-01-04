4a2a21fa0f0ca68dcd2884ecad27af5110851e51
Anuskota <anamangada.dev@gmail.com>
2024-01-04 23:09:58 +0100

    dreopdown home

--- src/components/molecules/navbarHomePage/NavbarHome.jsx
+++ src/components/molecules/navbarHomePage/NavbarHome.jsx
@@ -4,70 +4,147 @@ import logoWhite from "../../../assets/img/logo-white.png"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 const NavbarHome = () => {
   return (
     <nav>
-      <div className="header-wrapper fixed">
-        <div className="container-navbar">
-          <div className="row">
-            <div className="col-xl-12">
-              <div className="navigation">
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                  <a className="navbar-brand logo-white" href="#">
-                    <img src={logoWhite} alt="logo white covid" className="img-fluid" />
-                  </a>
-                  <a className="navbar-brand logo-dark" href="#">
-                    <img src={logoBlue} alt="logo blue covid" />
-                  </a>
-                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"/>
-                  </button>
-                  <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                      <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#"id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Home</a>
-                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                                <li><a className="dropdown-item" href="">Home One</a></li>
-                                                <li><a className="dropdown-item" href="">Home Two</a></li>
-                                                <li><a className="dropdown-item" href="">Home Three</a></li>
-                                                <li><a className="dropdown-item" href="">Home Four</a></li>
-                                                <li><a className="dropdown-item" href="">Home Five</a></li>
-                                                <li><a className="dropdown-item" href="">Home Dark One</a></li>
-                                                <li><a className="dropdown-item" href="">Home Dark Two</a></li>
-                                                <li><a className="dropdown-item" href="">Home Dark Three</a></li>
-                                                <li><a className="dropdown-item" href="">Home Dark Four</a></li>
-                                                <li><a className="dropdown-item" href="">Home dark Five</a></li>
-                                                <li><a className="dropdown-item" href="">Onepage</a></li>
-                                                <li><a className="dropdown-item" href="">Onepage Dark</a></li>
-                                                <li><a className="dropdown-item" href="">Tracker</a></li>
-                          </ul>
-                      </li>
-                      <li className="nav-item"><a className="nav-link" href="">Prevention</a></li>
-                      <li className="nav-item"><a className="nav-link" href="">Qurantine</a></li>
-                      <li className="nav-item dropdown">
-                                            <a className="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Pages</a>
-                                            <ul className="dropdown-menu">
-                                                <li><a className="dropdown-item" href="">Hand Wash</a></li>
-                                                <li><a className="dropdown-item" href="">SocialDistance</a></li>
-                                                <li> <a className="dropdown-item" href="">Blog</a></li>
-                                                <li><a className="dropdown-item" href="">Blog Details</a></li>
-                                            </ul>
-                      </li>
-                      <li className="nav-item"><a className="nav-link" href="">About</a></li>
-                      <li className="nav-item"><a className="nav-link" href="">Help</a></li>
-                    </ul>
-                  </div>
-                  <div className="singin-btn"></div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+         <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Home One
+                    </a>
+                  </li><li>
+                    <a className="dropdown-item" href="#">
+                      Home Two
+                    </a>
+                  </li><li>
+                    <a className="dropdown-item" href="#">
+                      Home Three
+                    </a>
+                  </li><li>
+                    <a className="dropdown-item" href="#">
+                      Home Four
+                    </a>
+                  </li><li>
+                    <a className="dropdown-item" href="#">
+                      Home Five
+                    </a>
+                  </li><li>
+                    <a className="dropdown-item" href="#">
+                      Home Dark One
+                    </a>
+                  </li><li>
+                    <a className="dropdown-item" href="#">
+                      Home Dark Two
+                    </a>
+                  </li><li>
+                    <a className="dropdown-item" href="#">
+                      Home Dark Three
+                    </a>
+                  </li><li>
+                    <a className="dropdown-item" href="#">
+                      Home Dark Four
+                    </a>
+                  </li><li>
+                    <a className="dropdown-item" href="#">
+                      Home Dark Five
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Onepage
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Onepage Dark
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Tracker
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              
 
-                </nav>
-              </div>
-            </div>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">
+                  Disabled
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
           </div>
         </div>
-      </div>  
-      
+      </nav>
     </nav>
   );
 };
 
 export default NavbarHome;
+
