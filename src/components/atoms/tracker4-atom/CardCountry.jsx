1b4560927c96cc4132225ba19ad3da4052b75506
Gabriela Farinha <vallefarinha@gmail.com>
2024-01-04 16:20:01 -0300

    adding active buttons

--- src/components/atoms/tracker4-atom/CardCountry.jsx
+++ src/components/atoms/tracker4-atom/CardCountry.jsx
@@ -1,24 +1,25 @@
-import PropTypes from 'prop-types';
-import './cardCountry.css';
+import PropTypes from "prop-types";
+import "./cardCountry.css";
 
-const CardCountry = ({ countryData, onClick }) => {
-    return (
-        <>
-            {countryData ? (
-                <button className='tracker4-card' onClick={() => onClick(countryData.countryInfo.iso2)}>
-                    <img src={countryData.countryInfo.flag} alt="country flag" />
-                    <h6>{countryData.country}</h6>
-                </button>
-            ) : (
-                <h1>Loading...</h1>
-            )}
-        </>
-    );
+const CardCountry = ({ countryData, onClick, className }) => {
+  return (
+    <>
+      {countryData ? (
+        <button className={`tracker4-card ${className}`} onClick={() => onClick(countryData.countryInfo.iso2)}>
+          <img src={countryData.countryInfo.flag} alt="country flag" />
+          <h6>{countryData.country}</h6>
+        </button>
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </>
+  );
 };
 
 CardCountry.propTypes = {
-    countryData: PropTypes.object,
-    onClick: PropTypes.func,
+  countryData: PropTypes.object,
+  onClick: PropTypes.func,
+  className: PropTypes.string,
 };
 
 export default CardCountry;
--- src/components/molecules/tracker4-mol/Tracker4Mol.jsx
+++ src/components/molecules/tracker4-mol/Tracker4Mol.jsx
@@ -1,14 +1,20 @@
-// Tracker4Mol.jsx
 import FetchData from "../../../services/FetchData";
 import CardCountry from "../../atoms/tracker4-atom/CardCountry";
 import CardData from "../../atoms/tracker4-atom/CardData";
 import UseData from '../../../hooks/UseData';
-import './tracker4Mol.css'
+import './tracker4Mol.css';
+import { useState } from "react";
 
 const Tracker4Mol = () => {
   const countryIds = ["US", "ES", "IT", "IN", "GB", "FR", "DE", "RU", "TR"];
   const { data } = FetchData("countries");
-  const [contentCard, handleClick] = UseData();  // Destructure the handleClick function
+  const [contentCard, handleClick] = UseData();
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleButtonClick = (index) => {
+    setActiveIndex(index);
+    handleClick();
+  };
 
   if (!data || data.length === 0 || !contentCard) {
     return <h1>Loading...</h1>;
@@ -18,12 +24,13 @@ const Tracker4Mol = () => {
     <>
       <h1>Covid-19 Country Wise - Tabs</h1>
       <div className="tracker4-container">
-        <div className="tracker4-row ">
+        <div className="tracker4-row">
           {countryIds.map((countryId, index) => (
             <CardCountry
               key={index}
               countryData={data.find((item) => item.countryInfo.iso2 === countryId)}
-              onClick={handleClick} 
+              onClick={() => handleButtonClick(index)}
+              className={`${activeIndex === index ? 'active' : ''}`}
             />
           ))}
         </div>
