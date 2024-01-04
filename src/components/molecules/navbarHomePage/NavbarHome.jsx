4daae47a60c14dad3110ae207e6c95aaf583a01f
Anuskota <anamangada.dev@gmail.com>
2024-01-04 17:54:59 +0100

    navbar features

--- src/components/molecules/navbarHomePage/NavbarHome.jsx
+++ src/components/molecules/navbarHomePage/NavbarHome.jsx
@@ -1,72 +1,73 @@
-import "./footer.css";
-import logo from "../../../assets/img/logo-white.png"
+import "./navbarHome.css";
+import logoBlue from "../../../assets/img/coronavirus.png"
+import logoWhite from "../../../assets/img/logo-white.png"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-const Footer = () => {
+const NavbarHome = () => {
   return (
-    <footer>
-        <div className="higher-footer">
-      <div className="icons-footer-container">
-        <div>
-          <img src={logo} alt="logo"></img>
+    <nav>
+      <div className="header-wrapper fixed">
+        <div className="container-navbar">
+          <div className="row">
+            <div className="col-xl-12">
+              <div className="navigation">
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                  <a className="navbar-brand logo-white" href="#">
+                    <img src={logoWhite} alt="logo white covid" className="img-fluid" />
+                  </a>
+                  <a className="navbar-brand logo-dark" href="#">
+                    <img src={logoBlue} alt="logo blue covid" />
+                  </a>
+                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"/>
+                  </button>
+                  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                      <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Home</a>
+                        <div className="dropdown-menu">
+                                                <a className="dropdown-item" href="">Home One</a>
+                                                <a className="dropdown-item" href="">Home Two</a>
+                                                <a className="dropdown-item" href="">Home Three</a>
+                                                <a className="dropdown-item" href="">Home Four</a>
+                                                <a className="dropdown-item" href="">Home Five</a>
+                                                <a className="dropdown-item" href="">Home Dark One</a>
+                                                <a className="dropdown-item" href="">Home Dark Two</a>
+                                                <a className="dropdown-item" href="">Home Dark Three</a>
+                                                <a className="dropdown-item" href="">Home Dark Four</a>
+                                                <a className="dropdown-item" href="">Home dark Five</a>
+                                                <a className="dropdown-item" href="">Onepage</a>
+                                                <a className="dropdown-item" href="">Onepage Dark</a>
+                                                <a className="dropdown-item" href="">Tracker</a>
+                                            </div>
+                      </li>
+                      <li className="nav-item"><a className="nav-link" href="">Prevention</a></li>
+                      <li className="nav-item"><a className="nav-link" href="">Qurantine</a></li>
+                      <li className="nav-item dropdown">
+                                            <a className="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Pages</a>
+                                            <div className="dropdown-menu">
+                                                <a className="dropdown-item" href="">Hand Wash</a>
+                                                <a className="dropdown-item" href="">SocialDistance</a>
+                                                <a className="dropdown-item" href="">Blog</a>
+                                                <a className="dropdown-item" href="">Blog Details</a>
+                                            </div>
+                      </li>
+                      <li className="nav-item"><a className="nav-link" href="">About</a></li>
+                      <li className="nav-item"><a className="nav-link" href="">Help</a></li>
+                    </ul>
+                  </div>
+                  <div className="singin-btn"></div>
+
+                </nav>
+              </div>
+            </div>
+          </div>
         </div>
-        <p>
-          These droplets can land on objects and surfaces around the person such
-          as tables, doorknobs and handrails.
-        </p>
-        <ul className="icons-list">
-          <li><i className="bi bi-facebook"></i></li>
-          <li><i className="bi bi-twitter"></i></li>
-          <li><i className="bi bi-linkedin"></i></li>
-          <li><i className="bi bi-youtube"></i></li>
-        </ul>
-      </div>
-      <div className="list-footer-container">
-        <div className="list-footer-content">
-          <h4>Quick Links</h4>
-          <ul>
-            <li>Prevention</li>
-            <li>Quarantine</li>
-            <li>About</li>
-            <li>Help</li>
-          </ul>
-        </div>
-        <div className="list-footer-content">
-          <h4>About</h4>
-          <ul>
-            <li>Hand Wash</li>
-            <li>Social Distance</li>
-            <li>Isolate</li>
-            <li>Difference</li>
-          </ul>
-        </div>
-        <div className="list-footer-content">
-          <h4>About</h4>
-          <ul>
-            <li>Hand Wash</li>
-            <li>Social Distance</li>
-            <li>Isolate</li>
-            <li>Difference</li>
-          </ul>
-        </div>
-        <div className="list-footer-content">
-          <h4>Help</h4>
-          <ul>
-            <li>Hand Wash</li>
-            <li>Social Distance</li>
-            <li>Isolate</li>
-            <li>Difference</li>
-          </ul>
-        </div>
-      </div>
-      </div>
-      <div className="lower-footer">
-        <p>© Copyright 2020. All Rights Reserved</p>
-        <p>Design by <a>DexignZone</a></p>
-      </div>     
-    </footer>
+      </div>  
+      
+    </nav>
   );
 };
 
-export default Footer;
+export default NavbarHome;
