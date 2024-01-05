19f7b5a3e01dd3c7ad1cc4f6e0d39e832e832755
Gabriela Farinha <vallefarinha@gmail.com>
2024-01-05 17:54:37 -0300

    adding USA data and blank div to insert the map

--- src/configuration/Router.jsx
+++ src/configuration/Router.jsx
@@ -3,6 +3,8 @@ import Home from "../pages/Home";
 import Tracker0 from "../pages/Tracker0";
 import Tracker3 from "../pages/Tracker3";
 import Tracker4 from "../pages/Tracker4";
+import Tracker7 from "../pages/Tracker7";
+
 
 function Router() {
   return (
@@ -12,6 +14,7 @@ function Router() {
         <Route path="/tracker" element={<Tracker0/>}></Route>
         <Route path="/tracker3" element={<Tracker3/>}></Route>
         <Route path="/tracker4" element={<Tracker4/>}></Route>
+        <Route path="/tracker7" element={<Tracker7/>}></Route>
     </Routes>
 </BrowserRouter>
   )
---
+++ src/pages/Tracker7.jsx
@@ -0,0 +1,16 @@
+
+import Navbar from '../components/molecules/navbarTracker/Navbar.jsx';
+import Tracker0Mol from '../components/molecules/tracker0-mol/Tracker0Mol.jsx';
+import Tracker7Mol from '../components/molecules/tracker7-mol/Tracker7Mol.jsx';
+
+const Tracker7 = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Tracker0Mol></Tracker0Mol>
+      <Tracker7Mol></Tracker7Mol>
+    </>
+  )
+}
+
+export default Tracker7;
