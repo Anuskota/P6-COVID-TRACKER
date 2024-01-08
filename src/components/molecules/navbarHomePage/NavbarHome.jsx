396cc01fabc6e6b327c1e8ed74675ffec979c310
Gabriela Farinha <vallefarinha@gmail.com>
2024-01-08 16:50:56 -0300

    removing overflow hidden

--- src/components/molecules/navbarHomePage/NavbarHome.jsx
+++ src/components/molecules/navbarHomePage/NavbarHome.jsx
@@ -2,8 +2,6 @@ import "./navbarHome.css";
 import logoBlue from "../../../assets/img/logo.png"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
-
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
--- src/components/molecules/navbarTracker/Navbar.jsx
+++ src/components/molecules/navbarTracker/Navbar.jsx
@@ -2,6 +2,7 @@ import "./navbar.css"
 import coronavirus from "../../../assets/img/coronavirus.png"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
@@ -11,20 +12,20 @@ const Navbar = () => {
                 <div className="nav-container">
                 <div>
                     <div className="logo">
-                        <a href="#"><img className="animate-spin" src={coronavirus} alt="coronavirus"/></a>
+                        <NavLink to="/"><img className="animate-spin" src={coronavirus} alt="coronavirus"/></NavLink>
                     </div>
                     <ul className="nav-items-container">
-                        <li><a href="#" className="active nav-content-bttn" data-tab="chats"><i className="bi bi-pie-chart"></i></a></li>
-                        <li><a href="#" className="nav-content-bttn" data-tab="table"><i className="bi bi-list"></i></a></li>
-                        <li><a href="#" className="nav-content-bttn" data-tab="tiles"><i className="bi bi-grid"></i></a></li>
-                        <li><a href="#" className="nav-content-bttn" data-tab="country-wise"><i className="bi bi-folder"></i></a></li>
-                        <li><a href="#" className="nav-content-bttn" data-tab="widgets"><i className="bi bi-layers"></i></a></li>
-                        <li><a href="#" className="nav-content-bttn" data-tab="charts"><i className="bi bi-graph-up-arrow"></i></a></li>
-                        <li><a href="#" className="nav-content-bttn" data-tab="continent" id="continentTab"><i className="bi bi-globe"></i></a></li>
-                        <li><a href="#" data-tab="blog" className="nav-content-bttn"><i className="bi bi-chat-left"></i></a></li>
+                        <li><NavLink to="/tracker" className="active nav-content-bttn" data-tab="chats"><i className="bi bi-pie-chart"></i></NavLink></li>
+                        <li><NavLink to="/tracker2" className="nav-content-bttn" data-tab="table"><i className="bi bi-list"></i></NavLink></li>
+                        <li><NavLink to="/tracker2" className="nav-content-bttn" data-tab="tiles"><i className="bi bi-grid"></i></NavLink></li>
+                        <li><NavLink to="/tracker3" className="nav-content-bttn" data-tab="country-wise"><i className="bi bi-folder"></i></NavLink ></li>
+                        <li><NavLink to="#" className="nav-content-bttn" data-tab="widgets"><i className="bi bi-layers"></i></NavLink></li>
+                        <li><NavLink to="#" className="nav-content-bttn" data-tab="charts"><i className="bi bi-graph-up-arrow"></i></NavLink></li>
+                        <li><NavLink to="#" className="nav-content-bttn" data-tab="continent" id="continentTab"><i className="bi bi-globe"></i></NavLink></li>
+                        <li><NavLink to="#" data-tab="blog" className="nav-content-bttn"><i className="bi bi-chat-left"></i></NavLink></li>
                     </ul>
                     </div>
-                    <div className="last-icon-nav"><li><a href="#" data-tab="blog" className="nav-content-bttn"><i className="bi bi-pause-circle"></i></a></li>
+                    <div className="last-icon-nav"><li><NavLink to="#" data-tab="blog" className="nav-content-bttn"><i className="bi bi-pause-circle"></i></NavLink></li>
                     </div>
 
                 </div>
