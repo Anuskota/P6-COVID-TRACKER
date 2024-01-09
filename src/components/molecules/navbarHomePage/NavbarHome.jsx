b085e82b4c27798fd5e32d90b457d46b0b5ed036
Anuskota <anamangada.dev@gmail.com>
2024-01-09 11:46:10 +0100

    navbar-toogle

--- src/components/molecules/navbarHomePage/NavbarHome.jsx
+++ src/components/molecules/navbarHomePage/NavbarHome.jsx
@@ -12,12 +12,17 @@ const NavbarHome = () => {
     <nav>
       <nav className=" navbar navbar-expand-lg bg-body navbar-light bg-light  ">
         <div className="container-fluid">
-          <div className="collapse navbar-collapse " id="navbarSupportedContent">
-            <div className="image-covid ">
+          <div className="image-covid ">
               <img className="ms-5" src={logoBlue}></img >
             </div>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+          <div className="collapse navbar-collapse " id="navbarSupportedContent">
+            
+            
             <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-grid gap-5 d-md-flex ">
-              <li className="nav-item dropdown ">
+              <li className="nav-item dropdown">
                 <a
                   className="nav-link dropdown-toggle"
                   href="#"
