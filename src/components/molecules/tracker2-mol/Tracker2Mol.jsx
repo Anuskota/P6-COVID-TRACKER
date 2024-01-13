f5345c1fde751e5b905f4b2f7ba43e7fd15d1546
Gabriela Farinha <vallefarinha@gmail.com>
2024-01-13 17:47:36 -0300

    adding datatable

--- src/components/molecules/tracker2-mol/Tracker2Mol.jsx
+++ src/components/molecules/tracker2-mol/Tracker2Mol.jsx
@@ -1,12 +1,31 @@
-import './tracker2Mol.css';
+import { DataTable } from '../../../../node_modules/primereact/datatable';
+import { Column } from '../../../../node_modules/primereact/column';
+import TopTen from '../../../services/TopTen';
+import { useState, useEffect } from 'react';
 
-const Tracker2Mol = () => {
 
-  return (
-    <>
-      <h1>Loading...</h1>
-    </>
-  );
+export default function Tracker2Mol() {
+  const {
+    topTenCases,
+  } = TopTen();
+
+
+  console.log(topTenCases)
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    topTenCases.getProductsMini().then(data => setProducts(data));
+  }, []);
+
+    return (
+        <div className="card">
+            <DataTable value={products} tableStyle={{ minWidth: '50rem' }}>
+                <Column field="code" header="Code"></Column>
+                <Column field="name" header="Name"></Column>
+                <Column field="category" header="Category"></Column>
+                <Column field="quantity" header="Quantity"></Column>
+            </DataTable>
+        </div>
+    );
 }
 
-export default Tracker2Mol;
