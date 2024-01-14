01152d25c97bf0a5b182bc71931b64323ca8b8a1
Gabriela Farinha <vallefarinha@gmail.com>
2024-01-14 17:34:28 -0300

    add table with styles

--- src/components/molecules/tracker2-mol/Tracker2Mol.jsx
+++ src/components/molecules/tracker2-mol/Tracker2Mol.jsx
@@ -1,18 +1,10 @@
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { useState } from 'react';
-import { FilterMatchMode } from 'primereact/api';
-import { InputText } from 'primereact/inputtext';
 import './tracker2Mol.css';
 import FetchData from '../../../services/FetchData';
-import "./tracker2Mol.css"
 
 export default function Tracker2Mol() {
   const { data, error } = FetchData('countries');
-  const [globalFilterValue, setGlobalFilterValue] = useState('');
-  const [filters, setFilters] = useState({
-    'country': { value: null, matchMode: FilterMatchMode.CONTAINS },
-});
 
   const renderFlag = (rowData) => {
     const flagUrl = rowData.countryInfo.flag;   
@@ -20,31 +12,9 @@ export default function Tracker2Mol() {
     return <img src={flagUrl} alt="Flag" style={{ width: '30px' }} />;
   };
 
-  const onGlobalFilterChange = (e) => {
-    const value = e.target.value;
-    let _filters = { ...filters };
-    _filters['global'].value = value;
-
-    setFilters(_filters);
-    setGlobalFilterValue(value);
-}
-  const renderHeader = () => {
-    return (
-        <div className="flex justify-content-end align-items-center">
-            <span className="p-input-icon-left">
-                <i className="pi pi-search" />
-                <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Keyword Search" />
-            </span>
-        </div>
-    )
-}
-
-const header = renderHeader();
-
   return (
     <div className="container-fluid">
-      <div className="row">
-      <div className=" tracker2-container">
+      <div className="row tracker2-container">
       <h1>Ajax Data Table - Covid-19 Country Wise State</h1>
       <div className="table">
         {error ? (
@@ -54,7 +24,6 @@ const header = renderHeader();
           value={data}
           paginator
           className="p-datatable-customers"
-          header={header}
           rows={10}
           paginatorTemplate=" CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
           currentPageReportTemplate="Showing {first} to {last} of {totalRecords} entries"
@@ -62,13 +31,10 @@ const header = renderHeader();
           dataKey="id"
           rowHover
           filterDisplay="menu"
-          scrollable
-          scrollHeight="400px"
-          scrollDirection="both"
           globalFilterFields={['country']}
           emptyMessage="No country found."
-        >
-           <Column field="flag" header="Flag"  body={renderFlag}  style={{ minWidth: '14rem' }}></Column>
+       >
+           <Column field="flag" header="Flag"  body={renderFlag}></Column>
             <Column field="country" header="Country" sortable/>
             <Column field="cases" header="Cases" sortable></Column>
             <Column field="todayCases" header="New Cases" sortable></Column>
@@ -84,7 +50,6 @@ const header = renderHeader();
         )}
       </div>
     </div>
-      </div>
     </div>
    
   );
